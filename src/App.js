347916e765a6452e724974ab2394eb02ed2d617f
marahmusleh <marahaltarefe18@gmail.com>
2021-08-01 18:40:53 +0300

    add a form and display info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locationData: {},
+    };
+  }
+
+  submitForm = async (e) => {
+    e.preventDefault();
+    const city = e.target.cityName.value;
+    const response = await axios.get(
+      `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${city}&format=json`
+    );
+    this.setState({
+      locationData: response.data[0],
+    });
+    console.log('our axios response', response.data[0]);
+  };
+  render() {
+    return (
+      <div>
+        <center>
+        <Form onSubmit={this.submitForm}>
+          <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+            <Form.Label>City Name</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter The City Name'
+              name='cityName'
+            />
+          </Form.Group>
+          <Button type='submit'>Explore!</Button>
+        </Form>
+        </center>
+        <center>
+        <Card style={{ width: '25rem' }}>
+          <Card.Img
+            variant='top'
+            src={`https://maps.locationiq.com/v3/staticmap?key=pk.b0acd25fa217904d671efabb56c53d66&q&center=${this.state.locationData.lat},${this.state.locationData.lon}&zoom=15`}
+          />
+          <Card.Body>
+            <Card.Title>Location information</Card.Title>
+            <Card.Text>
+              {this.state.locationData.display_name && 
+                <p>{this.state.locationData.display_name}</p>
+              }
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        </center>
+      </div>
+    );
+  }
 }
 
 export default App;
