5116166d6ffe654fbc0ad6bb8bff90d3bb8b6ff1
marahmusleh <marahaltarefe18@gmail.com>
2021-08-02 10:08:29 +0300

    changing

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export class App extends Component {
     try {
       const city = e.target.cityName.value;
       const response = await axios.get(
-        `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${city}&format=json`
+        `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${city}&format=json`
       );
       this.setState({
         locationData: response.data[0],
